c12448f01b3778c960c47a05eb5ee93bb239794d
Andrea Baretta <andrea.m.baretta@gmail.com>
2022-05-26 22:35:58 -0700

    Initial Minkowski diagram

--- test_scene.py
+++ test_scene.py
@@ -1,5 +1,6 @@
 from manim import *
 
+
 class SquareToCircle(Scene):
     def construct(self):
         circle = Circle()  # create a circle
@@ -9,5 +10,34 @@ class SquareToCircle(Scene):
         square.rotate(PI / 4)  # rotate a certain amount
 
         self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, circle))  # interpolate the square into the circle
+        # interpolate the square into the circle
+        self.play(Transform(square, circle))
         self.play(FadeOut(square))  # fade out animation
+
+
+class Animation(Scene):
+    def construct(self):
+        title = Title(
+            # spaces between braces to prevent SyntaxError
+            r"Minkowski Diagram of Twin Paradox",
+            include_underline=False,
+            font_size=40,
+        )
+
+        steveFrame = Axes(
+            x_range=[0, 10, 1],
+            y_range=[0, 10, 2],
+            x_length=5,
+            y_length=5,
+            tips=True,
+            axis_config={"stroke_color": WHITE},
+            x_axis_config={"include_ticks" : False, "include_numbers": False},
+        )
+        ryanGraphSteveFrame = steveFrame.plot(lambda x: x,
+                        x_range=[0, 10], use_smoothing=True, color=RED)
+
+        steveGraphSteveFrame = steveFrame.get_vertical_line(steveFrame.c2p(0, 10, 0), color=BLUE, line_config={"dashed_ratio": 1.5}, stroke_width=5)
+
+        self.add(title)
+        self.play(Create(steveFrame), run_time=2)
+        self.play(Create(ryanGraphSteveFrame), Create(steveGraphSteveFrame), run_time=2)
